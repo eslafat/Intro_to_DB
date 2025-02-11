b7aa7185ea88f9acee31ea124248f2c06208ab19
eslafat <109255696+eslafat@users.noreply.github.com>
2025-02-11 15:33:40 +0200

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -2,23 +2,23 @@
 USE alx_book_store;
 
 -- Create the Authors table
-CREATE TABLE AUTHORS (
+CREATE TABLE Authors (
     author_id INT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
 -- Create the Books table
-CREATE TABLE BOOKS (
+CREATE TABLE Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT NOT NULL,
     price DOUBLE,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 -- Create the Customers table
-CREATE TABLE CUSTOMERS (
+CREATE TABLE Customers (
     customer_id  INT  PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) UNIQUE,
@@ -26,19 +26,19 @@ CREATE TABLE CUSTOMERS (
 );
 
 -- Create the Orders table
-CREATE TABLE ORDERS (
+CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
     customer_id INT NOT NULL,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 -- Create the Order Details table
-CREATE TABLE ORDER_DETAILS (
+CREATE TABLE Order_details (
     orderdetail_id INT PRIMARY KEY,
     order_id INT NOT NULL,
     book_id INT NOT NULL,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
-    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
